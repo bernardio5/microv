7954fb91330452b3cb7d97b72cbeabb2f96b2b4e
bernardio5 <bernardio5@users.noreply.github.com>
2025-04-08 16:31:03 -0400

    LED version

--- microVision.js
+++ microVision.js
@@ -2,99 +2,70 @@
 
 function exSpriteCanvas(aCanvas, x0, y0) {
     this.canvas = aCanvas; 
-
     this.context = aCanvas.getContext("2d");
-    this.context.fillStyle = 'white';
-    this.context.strokeStyle = "#000";
     this.cxw = this.context.canvas.width; 
     this.cxh = this.context.canvas.height; 
-    this.tileSize = 32;    // size of tiles on screen; set how you please
-    this.imgTileSz = 32;    // sze of tiles in exTiles: 32
-    this.tileW = Math.floor(this.cxw / this.tileSize) + 1; // # tiles available on canvas in X
-    this.tileH = Math.floor(this.cxh / this.tileSize) + 1; // in Y
+    this.tileSize = 64;    // size of tiles on screen; set how you please
     this.x0 = x0 * this.tileSize;
     this.y0 = y0 * this.tileSize;
-
-    // load the tile set bitmap. 
-    this.loaded = 0; 
-    this.tiles = new Image(); 
-    this.tiles.src = "microTiles.png"; 
-    that = this;
-    this.tiles.onload= function(that) { that.loaded=1; }
-
-    // bbox for all draws since last erase-- uses canvas 
-    this.minX = 999.0; 
-    this.minY = 999.0; 
-    this.maxX = -999.0; 
-    this.maxY = -999.0; 
+    this.twopi = 2.0 * Math.PI;
 }
 
 
 exSpriteCanvas.prototype = {
     // draw tile at tx,ty at grid point x, y-- minimal default case
-    drawSprite: function(x, y, tx, ty) { 
-        var ts = this.tileSize; 
-        var is = this.imgTileSz;
+    drawSprite: function(x, y, c) { 
+        var ts = 40; 
         var xpos = this.x0 + (ts*x); 
         var ypos = this.y0 + (ts*y); 
-        this.context.drawImage(this.tiles, tx*is, ty*is, is, is, xpos, ypos, ts, ts); 
-        if (x<this.minX) { this.minX = x; }
-        if (y<this.minY) { this.minY = y; }
-        if (x>this.maxX) { this.maxX = x; }
-        if (y>this.maxY) { this.maxY = y; }
         
+        this.context.beginPath();
+        switch (c) { 
+        case 1: this.context.fillStyle = "#080000"; break;
+        case 2: this.context.fillStyle = "#000800"; break;
+        case 3: this.context.fillStyle = "#000008"; break;
+        case 4: this.context.fillStyle = "#080800"; break;
+        case 5: this.context.fillStyle = "#080008"; break;
+        case 6: this.context.fillStyle = "#000808"; break;
+        default: this.context.fillStyle = "#000008"; break;
+        }
+        this.context.arc(xpos, ypos, 0.6*ts, 0.0, this.twopi);
+        this.context.fill();
+
+        this.context.beginPath();
+        switch (c) { 
+        case 1: this.context.fillStyle = "#550000"; break;
+        case 2: this.context.fillStyle = "#005500"; break;
+        case 3: this.context.fillStyle = "#000055"; break;
+        case 4: this.context.fillStyle = "#555500"; break;
+        case 5: this.context.fillStyle = "#550055"; break;
+        case 6: this.context.fillStyle = "#005555"; break;
+        default: this.context.fillStyle = "#000055"; break;
+        }
+        this.context.arc(xpos, ypos, 0.25*ts, 0.0, this.twopi);
+        this.context.fill();
+
+        this.context.beginPath();
+        switch (c) { 
+        case 1: this.context.fillStyle = "#ff0000"; break;
+        case 2: this.context.fillStyle = "#00ff00"; break;
+        case 3: this.context.fillStyle = "#0000ff"; break;
+        case 4: this.context.fillStyle = "#ffff00"; break;
+        case 5: this.context.fillStyle = "#ff00ff"; break;
+        case 6: this.context.fillStyle = "#00ffff"; break;
+        default: this.context.fillStyle = "#0000ff"; break;
+        }
+        this.context.arc(xpos, ypos, 0.15*ts, 0.0, this.twopi);
+        this.context.fill();
+
     },
-
-
-    // draws multiple tiles on a grid point
-    drawLargeSprite: function(x, y, tx, ty, txsz, tysz) { 
-        var ts = this.tileSize; 
-        var is = this.imgTileSz;
-        var xpos = this.x0 + (ts*x); 
-        var ypos = this.y0 + (ts*y); 
-        this.context.drawImage(this.tiles, tx*is, ty*is, txsz*is, tysz*is, xpos, ypos, txsz*ts, tysz*ts); 
-        if (x<this.minX) { this.minX = x; }
-        if (y<this.minY) { this.minY = y; }
-        if ((x+txsz)>this.maxX) { this.maxX = (x+txsz); }
-        if ((y+tysz)>this.maxY) { this.maxY = (y+tysz); }
-    },
-
-
-    // scale the sprite by xscale and yscale 
-    drawStretchedSprite: function(x, y, tx, ty, txsz, tysz, xscale, yscale) { 
-        var ts = this.tileSize; 
-        var is = this.imgTileSz
-        var xpos = this.x0 + (ts*x); 
-        var ypos = this.y0 + (ts*y); 
-        this.context.drawImage(this.tiles, tx*is, ty*is, txsz*is, tysz*is, xpos, ypos, txsz*ts*xscale, tysz*ts*yscale); 
-        if (x<this.minX) { this.minX = x; }
-        if (y<this.minY) { this.minY = y; }
-        if (((x+txsz)*xscale)>this.maxX) { this.maxX = ((x+txsz)*xscale); }
-        if (((y+tysz)*yscale)>this.maxY) { this.maxY = ((y+tysz)*yscale); }
-    },
-
 
     // erases canvas
     clear: function() {
         var ts = this.tileSize; 
-        this.context.fillStyle = this.bg;
-        this.context.beginPath();
-        this.context.rect(this.x0 + (this.minX*ts), this.y0+(this.minY*ts), this.maxX*ts, this.maxY*ts);
-        this.context.fill();
-        this.minX = 999.0; 
-        this.minY = 999.0; 
-        this.maxX = -999.0; 
-        this.maxY = -999.0; 
-    },
-
-    // erases canvas
-    clearAll: function() {
-        var ts = this.tileSize; 
-        this.context.fillStyle = this.bg;
-        this.context.beginPath();
-        this.context.rect(0.0,0.0, this.tileW*ts, this.tileH*ts);
-        this.context.fill();
-    },
+        this.context.fillStyle = "black";
+        this.context.fillRect(0.0,0.0, this.cxw, this.cxh);
+    }
 }
 
 
@@ -134,7 +105,7 @@ function microVision(aCanvas) {
 
 microVision.prototype = {
     keyDn: function(evt) {
-        this.plot(15,14);
+        //this.plot(15,14);
         switch (evt.keyCode) {
             case 87: this.wDn = 2; break; 
             case 65: this.aDn = 2; break; 
@@ -149,7 +120,7 @@ microVision.prototype = {
     },
 
     keyUp: function(evt) {
-        this.plot(15,15);
+        //this.plot(15,15);
         switch (evt.keyCode) {
             case 87: this.wDn = 0; this.wTap = 1; break; 
             case 65: this.aDn = 0; this.aTap = 1; break; 
@@ -176,17 +147,21 @@ microVision.prototype = {
     },
 
     // draw tile at tx,ty at grid point x, y-- minimal default case
-    drawOne: function(x, y) {
-        this.sp.drawSprite(.9+(x*.5), 1.9+(y*.5), 15, 2);
+    drawOne: function(x, y, c) {
+        this.sp.drawSprite(.5+x, .5+y, c);
     },
 
+    plotc: function(x, y, c) {
+        var ind = (y*16)+x; 
+        this.pix[ind] = c; 
+    },
     plot: function(x, y) {
         var ind = (y*16)+x; 
-        this.pix[ind] = 2.0; 
+        this.pix[ind] = 1; 
     },
     unplot: function(x, y) {
         var ind = (y*16)+x; 
-        this.pix[ind] = 0.0; 
+        this.pix[ind] = 0; 
     },
 
     drawBlock: function(xlo, ylo, xsz, ysz) { 
@@ -208,13 +183,13 @@ microVision.prototype = {
     },
 
     redraw: function() {
-        this.sp.drawLargeSprite(0.0,0.0, 0,1, 11,15);
+        this.sp.clear();
         var i, j, ind; 
         for (i=0; i<16; i=i+1) { 
             for (j=0; j<16; j=j+1) { 
                 ind = (j*16)+i; 
-                if (this.pix[ind]>1.0) {
-                    this.drawOne(i, j); 
+                if (this.pix[ind]>0) {
+                    this.drawOne(i, j, this.pix[ind]); 
                 }
             }
         }
@@ -223,7 +198,7 @@ microVision.prototype = {
     wipe: function(ar) {
         var i; 
         for (i=0; i<256; i=i+1) { 
-            this.pix[i] = 0.0; 
+            this.pix[i] = 0; 
         }
     }
 }
@@ -240,6 +215,7 @@ function missile() {
     this.dx = 0;
     this.dy = 0;
     this.flying = 0; 
+    this.boomTimer = 0; 
 }
 
 
@@ -250,6 +226,7 @@ missile.prototype = {
         this.dx = dirx; 
         this.dy = diry;
         this.flying = 2; 
+        this.boomTimer = 0; 
     }, 
 
     move: function() { 
@@ -257,14 +234,22 @@ missile.prototype = {
             this.x = this.x + this.dx; 
             this.y = this.y + this.dy; 
         }
+        if (this.boomTimer>0.5) { 
+            this.boomTimer = this.boomTimer - 1; 
+        }
+    },
+
+    explode: function() { 
+        this.flying = 0; 
+        this.boomTimer = 20; 
     },
 
     hitWall: function() { 
         var res = 0; 
-        if ((this.x<.2)&&(this.dx<0)) { res = 2; }
-        if ((this.y<.2)&&(this.dy<0)) { res = 2; }
-        if ((this.x>14.8)&&(this.dx>0)) { res = 2; }
-        if ((this.y>14.8)&&(this.dy>0)) { res = 2; }
+        if ((this.x<-.8)&&(this.dx<0)) { res = 2; }
+        if ((this.y<-.8)&&(this.dy<0)) { res = 2; }
+        if ((this.x>15.8)&&(this.dx>0)) { res = 2; }
+        if ((this.y>15.8)&&(this.dy>0)) { res = 2; }
         return res; 
     },
 
@@ -284,6 +269,10 @@ missile.prototype = {
         sy = this.y-ay; 
         if (((sx*sx)+(sy*sy))<3) { res = 2; }
         return res; 
+    },
+
+    isExploding: function() {
+        return this.boomTimer > 0.5; 
     }
 }
 
